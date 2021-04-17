d21d014858a0a0267af8dcdd441af29516714206
0vishal <vishalsalaskar72@gmail.com>
2021-04-17 10:17:53 +0530

    Extend employeepayroll class with gender and date

--- Empwage.js
+++ Empwage.js
@@ -1,23 +1,31 @@
 class EmployeePayroll{
     id;
     salary;
+    gender;
+    startdate;
 
-    constructor(id,name,salary){
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+    constructor(...params){
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startdate=params[4];
     } 
 
     get name(){ return this._name;}
     set name(name){ this._name=name;}
 
     toString(){
-        return "id=" +this.id+ ", name= " +this.name+ ", salary= " +this.salary;
+        return "id=" +this.id+ ", name= " +this.name+ ", salary= " +this.salary+ " , gender= " +this.gender+
+        " , startdate= "+this.startdate;
     }
 }
 
-let employeePayroll = new EmployeePayroll(1,"Alice",20000);
+let employeePayroll = new EmployeePayroll(1,"rajan",80000);
 console.log(employeePayroll.toString());
 
-employeePayroll.name="Bob";
+employeePayroll.name="mahesh";
 console.log(employeePayroll.toString());
+
+let newemployeePayroll = new EmployeePayroll(2,"Shinu",45000,"F",new Date("23-09-2020"));
+console.log(newemployeePayroll.toString());
