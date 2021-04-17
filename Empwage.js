9ff67b011b0705ab9e9a03846258391d255bbb7f
0vishal <vishalsalaskar72@gmail.com>
2021-04-17 10:20:31 +0530

    Check name with Regex condition

--- Empwage.js
+++ Empwage.js
@@ -1,31 +1,42 @@
-class EmployeePayroll{
+class EmployeePayroll {
     id;
     salary;
     gender;
     startdate;
 
-    constructor(...params){
-        this.id=params[0];
-        this.name=params[1];
-        this.salary=params[2];
-        this.gender=params[3];
-        this.startdate=params[4];
-    } 
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startdate = params[4];
+    }
 
-    get name(){ return this._name;}
-    set name(name){ this._name=name;}
+    get name() { return this._name; }
+    set name(name) {
+        let regexname = RegExp('[A-Z]{1}[a-z]{3,}');
+        if (regexname.test(name)) {
+            this._name = name;
+        }
+        else {
+            throw "Wrong Name";
+        }
+    }
 
-    toString(){
-        return "id=" +this.id+ ", name= " +this.name+ ", salary= " +this.salary+ " , gender= " +this.gender+
-        " , startdate= "+this.startdate;
+    toString() {
+        return "id=" + this.id + ", name= " + this.name + ", salary= " + this.salary + " , gender= " + this.gender +
+            " , startdate= " + this.startdate;
     }
 }
 
-let employeePayroll = new EmployeePayroll(1,"rajan",80000);
+let employeePayroll = new EmployeePayroll(1, "shari", 10000);
 console.log(employeePayroll.toString());
+try {
+    employeePayroll.name = "Vishal";
+    console.log(employeePayroll.toString());
+} catch (e) {
+    console.error(e);
+}
 
-employeePayroll.name="mahesh";
-console.log(employeePayroll.toString());
-
-let newemployeePayroll = new EmployeePayroll(2,"Shinu",45000,"F",new Date("23-09-2020"));
+let newemployeePayroll = new EmployeePayroll(2, "Karesha", 78000, "F", new Date("09-10-2020"));
 console.log(newemployeePayroll.toString());
